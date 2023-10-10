b50e92aaa211e730613e4a3da365d9971a8135d3
Irina Batischeva <batischeva@bk.ru>
2023-10-10 22:10:24 +0300

    auth page layout done

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,38 @@ import { Context } from '../index';
 import '../style/Header.css';
 import logo from '../icon/header-logo.svg';
 import headerArrow from '../icon/header-arrow.svg';
+import { observer } from 'mobx-react-lite';
 
-const Header = () => {
+const Header = observer(() => {
   const {user} = useContext(Context);
   return (
     <header>
-      <div className='header-container'>
-        <span className='header-logo'>
-          <a href='#'>
-            <img src={logo} alt='Логотип Service Desk' />
-          </a>
-        </span>
-        <span>
-          <a href='#' className='header-company-link'>
-            ООО &laquo;Ленпромарматура&raquo;
-            <img src={headerArrow} />
-          </a>
-        </span>
-      </div>
+      {user.isAuth ?
+        <div className='header-container auth-header-container'>
+          <span>
+            <a href='#' className='header-company-link'>
+              ООО &laquo;Ленпромарматура&raquo;
+              <img src={headerArrow} />
+            </a>
+          </span>
+        </div>
+        :
+        <div className='header-container'>
+          <span className='header-logo'>
+            <a href='#'>
+              <img src={logo} alt='Логотип Service Desk' />
+            </a>
+          </span>
+          <span>
+            <a href='#' className='header-company-link'>
+              ООО &laquo;Ленпромарматура&raquo;
+              <img src={headerArrow} />
+            </a>
+          </span>
+        </div>
+      }
     </header>
   );
-};
+});
 
 export default Header;
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import '../style/Auth.css';
 
 const Auth = () => {
   return (
-    <div>
-      AUTH
-    </div>
+    <main className='container' style={{height: window.innerHeight - 124}}>
+      <div className='auth-block'>
+        <h1 className='auth-header'>
+          Вход в систему
+        </h1>
+        <form className='auth-form'>
+          <fieldset className='auth-form-fieldset'>
+            <input type='text' placeholder='Email' className='auth-form-input'/>
+            <input type='text' placeholder='Пароль' className='auth-form-input'/>
+          </fieldset>
+          <button className='form-btn'>
+            Войти
+          </button>
+        </form>
+      </div>
+    </main>
   );
 };
 
